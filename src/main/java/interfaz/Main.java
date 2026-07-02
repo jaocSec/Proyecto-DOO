187bb888c9c58f83be0881f0d14573ec2cc4de2b
jaocSec <jaoc2001@hotmail.com>
2026-07-02 16:54:29 -0400

    Refactor: Cambiado layout de la interfaz, implementado flatlaf

--- src/main/java/interfaz/Main.java
+++ src/main/java/interfaz/Main.java
@@ -1,9 +1,23 @@
 package interfaz;
 
 import javax.swing.*;
+import java.awt.Color;
+import com.formdev.flatlaf.FlatDarkLaf;
 
 public class Main {
     public static void main(String[] args){
+
+        try {
+            UIManager.put("Button.background", new Color(33, 37, 41));
+            UIManager.put("Button.foreground", new Color(242, 239, 225));
+            UIManager.put("Button.focusWidth", 0);
+
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception e) {
+            System.err.println("Error: Flatlaf");
+        }
+
+
         SwingUtilities.invokeLater(() -> {
             VentanaPrincipal ventana= new VentanaPrincipal();
             ventana.setVisible(true);
--- src/main/java/interfaz/VentanaPrincipal.java
+++ src/main/java/interfaz/VentanaPrincipal.java
@@ -9,9 +9,8 @@ public class VentanaPrincipal extends JFrame{
     private PanelMenu panelMenu;
 
     public VentanaPrincipal(){
-        setTitle("Sistema de reservas de clases particulares");
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setMinimumSize(new Dimension(800, 500));
+        setTitle("SRCP");
+        setMinimumSize(new Dimension(1000, 700));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
